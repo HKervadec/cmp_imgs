e53d47052de3f336a8bda86072c673c5eb0d8e53
Hoel Kervadec <hoel.kervadec@insa-rennes.fr>
2017-03-25 18:40:46 +0100

    Refactor resizing.

--- cmp_imgs.py
+++ cmp_imgs.py
@@ -7,7 +7,16 @@ from scipy.ndimage import imread
 def rgb2gray(img):
 	return np.dot(img, [0.299, 0.587, 0.114])
 
+def resize(img, shape=(64,64)):
+	resized = np.ndarray(shape)
+	for (i,j),_ in np.ndenumerate(resized):
+		f1 = gray_img.shape[0] // shape[0]
+		f2 = gray_img.shape[1] // shape[1]
 
+		slisse =  gray_img[i*f1:(i+1)*f1,j*f2:(j+1)*f2]
+		resized[i,j] = np.max(slisse)
+
+	return resized
 
 if __name__ == "__main__":
 	img_name = "1920x1080.jpg"
@@ -18,11 +27,6 @@ if __name__ == "__main__":
 	plt.imshow(gray_img, cmap=plt.cm.gray)
 	plt.show()
 
-	resized = np.ndarray((64, 64))
-	for (i,j),_ in np.ndenumerate(resized):
-		f1, f2 = gray_img.shape[0] // 64, gray_img.shape[1] // 64
-		slisse =  gray_img[i*f1:(i+1)*f1,j*f2:(j+1)*f2]
-		resized[i,j] = np.max(slisse)
-
+	resized = resize(gray_img)
 	plt.imshow(resized, cmap=plt.cm.gray)
 	plt.show()
